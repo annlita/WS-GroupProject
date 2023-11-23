18b2b97d30ead2fecd239f3c61e364ba0563aea5
Heremela Sitotaw <heremela.sitotaw@ontariotechu.net>
2023-11-23 12:03:08 -0500

    update

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("feedback");
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
-
